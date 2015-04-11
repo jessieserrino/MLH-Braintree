8cce02652d1aaba826ca4bdaf50f9f373d974375
Jessie Serrino <Jessie.Serrino@tufts.edu>
2015-04-11 22:17:11 +0100

    Minute changes, added cell UI files

---
+++ MLH-Braintree/CartTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CartTableViewCell.swift
+//  MLH-Braintree
+//
+//  Created by Jessie Serrino on 4/11/15.
+//  Copyright (c) 2015 Jessie Serrino. All rights reserved.
+//
+
+import UIKit
+
+class CartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
