66fed9361bc50f0dbdb32d8a8f7883916532d1df
Giovanni Alcantara <me@giovannialcantara.com>
2015-04-11 17:13:17 +0100

    Fixed Beacon class

--- MLH-Braintree/Beacon.swift
+++ MLH-Braintree/Beacon.swift
@@ -10,13 +10,13 @@ import Foundation
 
 class Beacon {
     var UUID: String
-    final var major
-    final var minor
+    final var major: Int
+    final var minor: Int
     
-    init(UUID: String, major: int, minor: int) {
-        self.UUID = String
-        self.major = major
+    init(UUID: String, major: Int, minor: Int) {
+        self.UUID = UUID
         self.minor = minor
+        self.major = major
     }
     
 }
