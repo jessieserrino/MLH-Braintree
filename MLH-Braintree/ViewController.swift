be0d3685382d13dcae17cbd530a6eb444e6e2318
Jessie Serrino <Jessie.Serrino@tufts.edu>
2015-04-11 14:01:51 +0100

    Initial Commit

---
+++ MLH-Braintree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLH-Braintree
+//
+//  Created by Jessie Serrino on 4/11/15.
+//  Copyright (c) 2015 Jessie Serrino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
