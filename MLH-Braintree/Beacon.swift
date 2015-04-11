8efb5660937495e130b827b02f6b0b1f8a26e234
Giovanni Alcantara <me@giovannialcantara.com>
2015-04-11 16:32:42 +0100

    Added model files

---
+++ MLH-Braintree/Beacon.swift
@@ -0,0 +1,22 @@
+//
+//  Beacon.swift
+//  MLH-Braintree
+//
+//  Created by Giovanni Alcantara on 11/04/2015.
+//  Copyright (c) 2015 Jessie Serrino. All rights reserved.
+//
+
+import Foundation
+
+class Beacon {
+    var UUID: String
+    final var major
+    final var minor
+    
+    init(UUID: String, major: int, minor: int) {
+        self.UUID = String
+        self.major = major
+        self.minor = minor
+    }
+    
+}
