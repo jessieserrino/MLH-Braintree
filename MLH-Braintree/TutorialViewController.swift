f791e29e91cf3052a61dcf3f0ffc6df13ccb3643
Jessie Serrino <Jessie.Serrino@tufts.edu>
2015-04-11 16:02:06 +0100

    Added view controllers

---
+++ MLH-Braintree/TutorialViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TutorialViewController.swift
+//  MLH-Braintree
+//
+//  Created by Jessie Serrino on 4/11/15.
+//  Copyright (c) 2015 Jessie Serrino. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
