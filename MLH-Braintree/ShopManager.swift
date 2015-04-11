9cba94ec007ea0a1cf2810551786111ebe0036cb
Jessie Serrino <Jessie.Serrino@tufts.edu>
2015-04-11 22:48:08 +0100

    Shop Manager

---
+++ MLH-Braintree/ShopManager.swift
@@ -0,0 +1,14 @@
+//
+//  ShopManager.swift
+//  MLH-Braintree
+//
+//  Created by Jessie Serrino on 4/11/15.
+//  Copyright (c) 2015 Jessie Serrino. All rights reserved.
+//
+
+import UIKit
+
+class ShopManager: NSObject {
+   
+
+}
